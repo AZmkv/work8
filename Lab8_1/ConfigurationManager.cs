1b54ee1df4ed499dbd66711f18b415e1c0ea0336
AZmkv <144830991+AZmkv@users.noreply.github.com>
2023-11-21 16:06:58 +0200

    Add files via upload

---
+++ Lab8_1/ConfigurationManager.cs
@@ -0,0 +1,43 @@
+﻿namespace Lab8_1
+{
+    class ConfigurationManager
+    {
+        private static ConfigurationManager instance;
+        private string loggingMode;
+        private string databaseConnection;
+
+        private ConfigurationManager()
+        {
+            loggingMode = "DefaultLogging";
+            databaseConnection = "DefaultConnection";
+        }
+        public static ConfigurationManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ConfigurationManager();
+                }
+                return instance;
+            }
+        }
+        public void DisplayConfiguration()
+        {
+            Console.WriteLine($"Logging Mode: {loggingMode}");
+            Console.WriteLine($"Database Connection: {databaseConnection}");
+        }
+        public void ChangeLoggingMode(string newLoggingMode)
+        {
+            loggingMode = newLoggingMode;
+        }
+        public void ChangeDatabaseConnection(string newConnection)
+        {
+            databaseConnection = newConnection;
+        }
+        public void SaveConfiguration()
+        {
+            Console.WriteLine("Configuration saved.");
+        }
+    }
+}
---
+++ Lab8_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab8_1;
+
+class Program
+{
+    static void Main()
+    {
+        ConfigurationManager configManager = ConfigurationManager.Instance;
+        //конфігураційні налаштування
+        configManager.DisplayConfiguration();
+
+        Console.Write("Enter new logging mode: ");
+        string newLoggingMode = Console.ReadLine();
+        configManager.ChangeLoggingMode(newLoggingMode);
+
+        Console.Write("Enter new database connection: ");
+        string newDatabaseConnection = Console.ReadLine();
+        configManager.ChangeDatabaseConnection(newDatabaseConnection);
+
+        configManager.SaveConfiguration();
+
+        configManager.DisplayConfiguration();
+    }
+}
